1f6da071a6fb92f498e74e62c2b2b4c6a163b17b
Hyunseung <dev.hsyang@gmail.com>
2023-07-31 11:50:52 +0900

    add: querydsl config 파일

---
+++ src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/config/QuerydslConfig.java
@@ -0,0 +1,18 @@
+package com.openpromt.coffeee.swf2023.openpromtserver.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+
+public class QuerydslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+
+        return new JPAQueryFactory(em);
+    }
+}
