3fd1b36b935c02c609330f10d7bf6bf9fe05e449
Hyunseung <dev.hsyang@gmail.com>
2023-07-31 10:32:11 +0900

    init repo

---
+++ src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/OpenpromtServerApplication.java
@@ -0,0 +1,13 @@
+package com.openpromt.coffeee.swf2023.openpromtserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenpromtServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenpromtServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openpromt/coffeee/swf2023/openpromtserver/OpenpromtServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openpromt.coffeee.swf2023.openpromtserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenpromtServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
