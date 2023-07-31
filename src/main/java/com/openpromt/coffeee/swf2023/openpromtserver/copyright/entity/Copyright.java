e96cdb09a7d25de83c41303eee620586531a1b59
Hyunseung <dev.hsyang@gmail.com>
2023-07-31 11:41:26 +0900

    add: copyright entity

---
+++ src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/copyright/entity/Copyright.java
@@ -0,0 +1,24 @@
+package com.openpromt.coffeee.swf2023.openpromtserver.copyright.entity;
+
+import com.openpromt.coffeee.swf2023.openpromtserver.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Copyright {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long copyright_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+    private String priv_key;
+    private String pub_key;
+}
