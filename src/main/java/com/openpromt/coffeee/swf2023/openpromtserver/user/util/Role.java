893826d9c356814e30d3f325fed927f9c3ab4daa
Hyunseung <dev.hsyang@gmail.com>
2023-07-31 11:43:41 +0900

    add: user단 Role Enum 추가

---
+++ src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/user/util/Role.java
@@ -0,0 +1,16 @@
+package com.openpromt.coffeee.swf2023.openpromtserver.user.util;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    ADMIN("ROLE_ADMIN", "관리자"),
+    USER("ROLE_USER", "사용자"),
+    GUEST("ROLE_GUEST", "미인증사용자");
+
+    private final String key;
+    private final String title;
+}
