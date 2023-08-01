2c6f953c43dc7b80a41cdc7c5b3b222ce66d3717
박종건 <gun5046@naver.com>
2023-08-01 21:53:38 +0900

    Fix: WebConfiguration allowedOriginPattern 수정

--- src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/config/WebConfiguration.java
+++ src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/config/WebConfiguration.java
@@ -23,7 +23,6 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .allowedOrigins("http://localhost:3000")
-                .allowedOriginPatterns("*")
                 .allowedMethods("*")
 //			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                 .maxAge(1800);
