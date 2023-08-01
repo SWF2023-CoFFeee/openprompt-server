fe3f25b5b368f86e99bdfd107c234b5c685d771d
박종건 <gun5046@naver.com>
2023-08-01 10:20:00 +0900

    Feat: Swagger Configuration 추가

---
+++ src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/config/SwaggerConfiguration.java
@@ -0,0 +1,43 @@
+package com.openpromt.coffeee.swf2023.openpromtserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket apiV1() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+
+//                        basePackage("com.hanjan.user"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Coffeee",
+                "Seoul Web3.0 Festival",
+                "version1.0",
+                "",
+                new Contact("Contact Me", "", ""),
+                "Edit Licenses",
+                "",
+                new ArrayList<>()
+        );
+    }
+}
