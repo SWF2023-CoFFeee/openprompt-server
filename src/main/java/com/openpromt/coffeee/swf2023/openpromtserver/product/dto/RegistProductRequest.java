8baebe5d9ca584c97d1dd5011a39154bbc03d59c
박종건 <gun5046@naver.com>
2023-08-01 16:44:06 +0900

    Feat: Google Cloud Storage 추가

---
+++ src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/product/dto/RegistProductRequest.java
@@ -0,0 +1,21 @@
+package com.openpromt.coffeee.swf2023.openpromtserver.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistProductRequest {
+    private String copyright_id;
+    private String product_title;
+    private String description;
+    private String product_type;
+    private MultipartFile thumbnail;
+    private int price;
+    private String AI_type;
+    private String seller_addr;
+}
---
+++ src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/util/googlestorage/GoogleStorageUtil.java
@@ -0,0 +1,40 @@
+package com.openpromt.coffeee.swf2023.openpromtserver.util.googlestorage;
+
+import com.google.cloud.storage.Acl;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class GoogleStorageUtil {
+    @Value("${spring.cloud.gcp.storage.bucket}")
+    private static String bucketName;
+
+    private static final String baseUrl = "https://storage.googleapis.com/coffeee/";
+    private static Storage storage;
+
+    public static String getGoogleStorageUrl(MultipartFile file) throws IOException {
+        String uuid = UUID.randomUUID().toString(); // GCS에 저장될 파일 이름
+        String type = file.getContentType(); // 파일 형식
+        BlobInfo blobInfo = storage.create(
+                BlobInfo.newBuilder(bucketName, uuid)
+                        .setAcl(new ArrayList<>(Arrays.asList(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))))
+                        .setContentType(type)
+                        .build(),
+                file.getInputStream()
+        );
+        return baseUrl + uuid;
+    }
+
+}
