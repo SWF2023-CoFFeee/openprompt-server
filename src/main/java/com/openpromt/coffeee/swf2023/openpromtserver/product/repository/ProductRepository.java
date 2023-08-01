49bea38db8c9a675877817f967c1fb79fe8a87fb
hyunseung <dev.hsyang@gmail.com>
2023-08-01 23:34:04 +0900

    chore: 수정

--- src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/product/repository/ProductRepository.java
+++ src/main/java/com/openpromt/coffeee/swf2023/openpromtserver/product/repository/ProductRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
     List<Product> findAllByProductType(String product_type);
 
     List<Product> findByCopyrightId(String copyright_id);
